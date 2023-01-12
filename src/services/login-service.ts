f53fc3e9f28164ff330d9e1f400e1b5927825f0b
omarhosny206 <omarhosny102@gmail.com>
2023-01-12 21:16:35 +0200

    Formatted the log-service file

--- src/services/login-service.ts
+++ src/services/login-service.ts
@@ -7,24 +7,24 @@ import ApiError from "../utils/api-error";
 import * as jwt from "../utils/jwt";
 
 export const login = async (userLogin: ILoginRequest): Promise<ILoginResponse> => {
-    try {
-        const {email, password} = userLogin;
-        const storedUser = await userService.getByEmail(email);
+  try {
+    const { email, password } = userLogin;
+    const storedUser = await userService.getByEmail(email);
 
-        if (!storedUser) {
-            throw ApiError.unauthorized("Bad Credentials: Invalid email");
-        }
-
-        const hashedPassword = storedUser.password;
-        const areEqualPasswords = await bcrypt.compare(password, hashedPassword);
-
-        if (!areEqualPasswords) {
-            throw ApiError.unauthorized("Bad Credentials: Invalid password");
-        }
-
-        const token = await jwt.generate(email);
-        return {user: storedUser, token: token};
-    } catch (error) {
-        throw ApiError.from(error);
+    if (!storedUser) {
+      throw ApiError.unauthorized("Bad Credentials: Invalid email");
     }
+
+    const hashedPassword = storedUser.password;
+    const areEqualPasswords = await bcrypt.compare(password, hashedPassword);
+
+    if (!areEqualPasswords) {
+      throw ApiError.unauthorized("Bad Credentials: Invalid password");
+    }
+
+    const token = await jwt.generate(email);
+    return { user: storedUser, token: token };
+  } catch (error) {
+    throw ApiError.from(error);
+  }
 };
